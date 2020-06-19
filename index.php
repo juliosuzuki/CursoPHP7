cf1ee9ff2dcc746483d078ed26768ed333df9dee
julio suzuki <suzukij332@gmail.com>
2020-06-18 23:30:57 -0300

    Foi criada os método de listas

--- index.php
+++ index.php
@@ -8,11 +8,23 @@ require_once("config.php");
 // $usuario = $sql-> select("SELECT *FROM tb_usuario");
 
 // echo  json_encode($usuario);
+# carrega um usuario
+// $root = new Usuario();
+// $root->loadById(3);
 
-$root = new Usuario();
-$root->loadById(3);
+//echo $root;
+#chamou o metdo especifico para getLIST//
+// $lista = Usuario::getList();
 
-echo $root;
+// echo json_encode($lista);
 
+// $search = Usuario::search("ma");
+// echo json_encode($search);
 
+#carregando um usuario com o login e senha
+   $usuario = new Usuario();
+
+   $usuario->login("maquina","10011000");
+
+   echo $usuario;
  ?>
